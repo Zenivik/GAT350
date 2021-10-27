4022f59c9d139e548e1197789725408dc9fc406c
Louis Bui <lbui@student.neumont.edu>
2021-10-27 09:59:11 -0600

    km

--- RayTracer/Types.h
+++ RayTracer/Types.h
@@ -15,14 +15,10 @@ inline color_t Vec3ToColor(const glm::vec3& vec3)
 {
     color_t color;
 
-
-
     color.r = static_cast<std::uint8_t>(glm::clamp(vec3[0], 0.0f, 1.0f) * 255);
     color.g = static_cast<std::uint8_t>(glm::clamp(vec3[1], 0.0f, 1.0f) * 255);
     color.b = static_cast<std::uint8_t>(glm::clamp(vec3[2], 0.0f, 1.0f) * 255);
     color.a = 0;
-
-
 
     return color;
 }
@@ -35,4 +31,25 @@ inline float dot(const glm::vec3& v1, const glm::vec3& v2)
 inline float angle(const glm::vec3& v1, const glm::vec3& v2)
 {
     return glm::acos(dot(v1, v2));
+}
+
+inline float random01()
+{
+    return rand() / (float)RAND_MAX;
+}
+
+inline float random(float min, float max)
+{
+    return min + (max - min) * random01();
+}
+
+inline glm::vec3 randomInUnitSphere()
+{
+    glm::vec3 p;
+    do
+    {
+        p = glm::vec3{ random(-1, 1), random(-1, 1), random(-1, 1) };
+    } while (glm::length2(p) >= 1);
+
+    return p;
 }
