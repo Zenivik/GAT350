4d526a09da8d4e97ce215edc52f49bd464ab6caf
Louis Bui <lbui@student.neumont.edu>
2021-10-30 14:16:39 -0600

    10/30/21 Camera

--- RayTracer/Main.cpp
+++ RayTracer/Main.cpp
@@ -24,12 +24,17 @@ int main(int, char**)
 	// scene
 	std::unique_ptr<Scene> scene = std::make_unique<Scene>();
 
+	// camera
+	float focalLength = glm::length(glm::vec3{ 5, 5, 5 } - glm::vec3{ 0, 0, -10 });
+	std::unique_ptr<Camera> camera = std::make_unique<Camera>(glm::vec3{ 5, 5, 5 }, glm::vec3{ 0, 0, -10 }, glm::vec3{ 0, 1, 0 }, 90.0f, 
+		glm::ivec2{ framebuffer->colorBuffer.width, framebuffer->colorBuffer.height }, 1.0f, focalLength);
+	
 	scene->Add(std::move(std::make_unique<Sphere>(glm::vec3{ 0, 0, -10 }, 3.0f, std::make_shared<Lambertian>(glm::vec3{ 1, 0, 0 }))));
 	scene->Add(std::move(std::make_unique<Sphere>(glm::vec3{ 3, 3, -8 }, 1.0f, std::make_shared<Metal>(glm::vec3{ 0, 1, 0 }, 0.0f))));
 	scene->Add(std::move(std::make_unique<Plane>(glm::vec3{ 0, -3, 0 }, glm::vec3{ 0, 1, 0 }, std::make_shared<Lambertian>(glm::vec3{ 0.5f, 0.5f, 0.5f }))));
 
 	framebuffer->Clear({ 0, 0, 0, 0 });
-	tracer->Trace(framebuffer->colorBuffer, scene.get());
+	tracer->Trace(framebuffer->colorBuffer, scene.get(), camera.get());
 	framebuffer->Update();
 
 	bool quit = false;
--- RayTracer/Material.cpp
+++ RayTracer/Material.cpp
@@ -2,8 +2,8 @@
 
 bool Lambertian::Scatter(const ray_t& ray, const raycastHit_t& hit, glm::vec3& attenuation, ray_t& scattered) const
 {
-    glm::vec3 target = hit.point + hit.normal + randomInUnitSphere();
-    glm::vec3 direction = glm::normalize(target - hit.point);
+    //glm::vec3 target = hit.point + hit.normal + randomInUnitSphere();
+    glm::vec3 direction = glm::normalize(hit.normal + randomInUnitSphere());
     scattered = { hit.point, direction };
     attenuation = albedo;
 
@@ -15,8 +15,7 @@ bool Metal::Scatter(const ray_t& ray, const raycastHit_t& hit, glm::vec3& attenu
 {
     glm::vec3 reflected = reflect(glm::normalize(ray.direction), hit.normal);
 
-    scattered.origin = hit.point;
-    scattered.direction = reflected + (randomInUnitSphere() * fuzz);
+    scattered = ray_t{ hit.point, reflected + (randomInUnitSphere() * fuzz) };
     attenuation = albedo;
 
     return glm::dot(scattered.direction, hit.normal) > 0;
